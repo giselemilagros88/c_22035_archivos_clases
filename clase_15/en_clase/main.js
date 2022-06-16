bee145b4a790f1819653e7ee86f7eaf8bc09a0e5
gmgonzalez88 <gis.gonzalez@bue.edu.ar>
2022-06-16 16:49:39 -0300

    subida de clases

---
+++ clase_15/en_clase/main.js
@@ -0,0 +1,31 @@
+   // todas mis funciones
+   function saludar() {
+    alert("Hola Mundo!");
+}
+// Declaración de variables
+/* Comentarios de varias lineas
+  esto tambien es un comentario*/
+var miNombre = "Pedro";
+var miEdad = 23  ;
+console.log(miNombre);
+console.log('Esta es mi Edad: ' + miEdad);
+var miVariable ;
+console.log(miVariable);
+
+var miVariable2 ;
+// console.log(mivariable2);
+// concatenar en una variable
+console.log('4' + '5');
+
+console.log( '1' + 1);
+miSuma = 5;
+var miConcatenacion = 'Hola ' + 'Mundo ' + (33 + 1 + 6) + ' ' +miSuma;
+var miSuma = 1 + 1 + 7 + 8;
+miSuma=" Chau";
+console.log(miConcatenacion);
+console.log(miSuma);
+
+var nuevaVariable = miConcatenacion + ' / ' + miSuma;
+console.log(nuevaVariable);
+
+document.write("Chau Chicos!!");
---
+++ clase_16/en_clase/main.js
@@ -0,0 +1,87 @@
+var unaVariable ;
+
+console.log(unaVariable);
+
+unaVariable = "Hola Mundo" ;
+
+console.log(unaVariable);
+
+unaVariable = true ;
+
+console.log(unaVariable);
+
+unaVariable =  1 > 10;
+
+console.log(unaVariable);
+
+
+var a = 5; 
+var b = "hola";
+var b = a;
+
+console.log(a);
+console.log(b);
+
+
+let g = 15;
+console.log(g);
+
+g = "gisele";
+console.log(g);
+
+
+const soyConstante = 3;
+console.log(soyConstante);
+
+var z = 1;
+var z = "Jose";
+console.log(z);
+
+let y = "hola";
+/*let y = "chau";*/
+
+
+const nombre = z ;
+
+console.log(nombre);
+
+var numeroString= "5";
+var numeroString2 = "10";
+
+var suma = numeroString +"  " + numeroString2;
+console.log(suma);
+
+var suma = parseInt(numeroString) + parseInt(numeroString2);
+
+console.log(suma);
+
+/*alert(suma);*/
+
+var miLetra = "5.66b7";
+miLetra = parseInt(miLetra);
+
+console.log(miLetra);
+
+var miFloat = 2.6;
+var miFloat = 2.6 + 1 ;
+console.log(parseInt(miFloat)); // me trunca el float a entero
+
+var miFrase = "Hola, Soy Gisele";
+
+miFrase = parseInt(miFrase);
+
+console.log(miFrase);
+
+var numeroFloat= "3.56";
+console.log( parseFloat(numeroFloat) );
+
+var myNumberValue = +"8"; // number 8
+console.log(myNumberValue); 
+
+myNumberValue = ++myNumberValue; // number 9
+console.log(myNumberValue);
+
+
+
+
+      
